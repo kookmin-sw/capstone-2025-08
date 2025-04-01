c75876f2a7730b7e69af4412ab33cc77dafe1361
TaegeunYou <dbxorms2310@naver.com>
2025-04-01 17:13:17 +0900

    [BE] feat: 모델 서버 백그라운드 태스크 설정

--- ai/main.py
+++ ai/main.py
@@ -1,4 +1,17 @@
+import threading
+from config import get_settings
+from core.sqs import process_message
 from app import app
+@app.on_event("startup")
+def startup_event():
+    app.state.settings = get_settings()
+    """FastAPI 서버 시작 시 백그라운드 태스크 실행"""
+    start_background_task()
+
+def start_background_task():
+    """백그라운드에서 `process_message()` 실행"""
+    thread = threading.Thread(target=process_message, daemon=True)
+    thread.start()
 
 @app.get("/")
 def hello_world():
