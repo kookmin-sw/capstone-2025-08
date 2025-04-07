50c34aca577fdb27922bb146d352db70c440089c
ukly <dlwjddnr5438@kookmin.ac.kr>
2025-04-07 13:39:21 +0900

    [BE] fix: annotation 폴더 위치 변경

--- backend/src/main/java/site/pathos/domain/annotation/cellAnnotation/entity/CellAnnotation.java
+++ backend/src/main/java/site/pathos/domain/annotation/cellAnnotation/entity/CellAnnotation.java
@@ -1,4 +1,4 @@
-package site.pathos.domain.annotation.tissueAnnotation.entity;
+package site.pathos.domain.annotation.cellAnnotation.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
--- backend/src/main/java/site/pathos/domain/annotation/tissueAnnotation/entity/TissueAnnotation.java
+++ backend/src/main/java/site/pathos/domain/annotation/tissueAnnotation/entity/TissueAnnotation.java
@@ -1,4 +1,4 @@
-package site.pathos.domain.annotation.cellAnnotation.entity;
+package site.pathos.domain.annotation.tissueAnnotation.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
