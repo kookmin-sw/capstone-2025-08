ab070137f6ad9f96e516508bd92e34ef1ea98bd2
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2025-04-11 03:07:38 +0900

    [FE] refactor: 어노테이션 관련 타입 정의

---
+++ frontend/src/types/annotation.ts
@@ -0,0 +1,31 @@
+export interface Point {
+  x: number;
+  y: number;
+}
+
+export interface Stroke {
+  points: Point[];
+  color: string;
+  size: number;
+  isEraser?: boolean;
+}
+
+export interface ROI {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+export interface MaskTile {
+  img: HTMLImageElement;
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+}
+
+export interface LoadedROI {
+  bbox: { x: number; y: number; w: number; h: number };
+  tiles: MaskTile[];
+}
