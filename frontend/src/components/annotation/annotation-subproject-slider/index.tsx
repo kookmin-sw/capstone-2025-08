0ad7b860e973841f69fc9eee9db6bf05768002c6
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2025-04-21 18:25:06 +0900

    [FE] WIP: 서브프로젝트 슬라이더 임시 구현

--- frontend/src/components/annotation/annotation-subproject-slider/index.tsx
+++ frontend/src/components/annotation/annotation-subproject-slider/index.tsx
@@ -1,5 +1,31 @@
-export default function AnnotationSubProjectSlider() {
+import { SubProject } from '@/types/project-schema';
+
+interface Props {
+  subProjects: SubProject[];
+  selected: SubProject | null;
+  onSelect: (sp: SubProject) => void;
+}
+
+export default function AnnotationSubProjectSlider({
+  subProjects,
+  selected,
+  onSelect,
+}: Props) {
   return (
-    <div className="h-full w-full bg-red-500"> 서브프로젝트 슬라이더 </div>
+    <div className="flex w-full space-x-4 overflow-x-auto bg-gray-100 px-6 py-4">
+      {subProjects.map((sp) => (
+        <div
+          key={sp.id}
+          onClick={() => onSelect(sp)}
+          className={`flex h-[80px] min-w-[120px] cursor-pointer items-center justify-center rounded-md border text-sm ${
+            selected?.id === sp.id
+              ? 'border-blue-800 bg-blue-600 text-white'
+              : 'bg-white text-gray-700'
+          }`}
+        >
+          Sub #{sp.id}
+        </div>
+      ))}
+    </div>
   );
 }
