42ded05857f76256a32ad0c2bc64cefcba072ae8
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2025-04-22 06:09:30 +0900

    [FE] feat: 각 페이지 타이틀 컴포넌트 구현

---
+++ frontend/src/components/common/page-title/index.tsx
@@ -0,0 +1,37 @@
+import { Button } from '@/components/ui/button';
+import { type ReactNode } from 'react';
+
+export default function PageTitle({
+  title,
+  icon,
+  buttonName,
+  buttonVariant = 'default',
+  buttonSize = '32',
+  onButtonClick,
+}: {
+  title: string;
+  icon?: ReactNode;
+  buttonName?: string;
+  buttonSize?: string;
+  buttonVariant?: 'default' | 'outline';
+  onButtonClick?: () => void;
+}) {
+  return (
+    <>
+      <div className="flex flex-row items-center justify-between">
+        <h1 className="text-3xl font-bold">{title}</h1>
+        {buttonName && (
+          <Button
+            variant={buttonVariant}
+            className={`flex items-center gap-2 w-${buttonSize}`}
+            onClick={onButtonClick}
+          >
+            {icon}
+            {buttonName}
+          </Button>
+        )}
+      </div>
+      <div className="my-8 border-b" />
+    </>
+  );
+}
