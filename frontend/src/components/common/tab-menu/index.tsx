23cd5b748b2bb7c68ac90fd4834027b1508e953b
hyeonjin6530 <cola1876@naver.com>
2025-05-05 22:32:49 +0900

    [FE] design: 활성화된 tab background color 수정

--- frontend/src/components/common/tab-menu/index.tsx
+++ frontend/src/components/common/tab-menu/index.tsx
@@ -7,7 +7,7 @@ export interface TabsProps {
 }
 
 const tabTriggerClass =
-  'h-full rounded-b-none rounded-t-sm px-4 py-2 text-sm data-[state=active]:bg-black data-[state=active]:text-white cursor-pointer';
+  'h-full rounded-b-none rounded-t-sm px-4 py-2 text-sm data-[state=active]:bg-primary data-[state=active]:text-white cursor-pointer';
 
 export default function TabMenu({ tabs, defaultValue }: TabsProps) {
   return (
