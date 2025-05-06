8633c6842d30a28b954a26dd8dd7a36e601d942d
hyeonjin6530 <cola1876@naver.com>
2025-05-07 02:38:27 +0900

    [FE] fix: hover시, cursor-pointer 에러 수정

--- frontend/src/components/sidebar/sidebar-top-menu/index.tsx
+++ frontend/src/components/sidebar/sidebar-top-menu/index.tsx
@@ -92,7 +92,7 @@ export function TopMenu() {
                     onClick={() => router.push(item.items[0].url)}
                   >
                     <SidebarMenuButton
-                      className={`${
+                      className={`cursor-pointer ${
                         pathname.startsWith(item.url)
                           ? 'bg-sidebar-accent-foreground text-sidebar-accent font-medium'
                           : 'hover:bg-sidebar-accent hover:text-sidebar-accent-foreground'
