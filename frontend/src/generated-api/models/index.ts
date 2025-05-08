9d1a7604b3b13560d0786c2c379bf83e0a47c210
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2025-05-08 23:00:50 +0900

    [FE] feat: openapi-generator 통해 생성된 api end-point, type 파일들 추가

---
+++ frontend/src/generated-api/models/index.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AnnotationHistoryResponseDto';
+export * from './AnnotationHistorySummaryDto';
+export * from './CellDetail';
+export * from './CreateProjectRequestDto';
+export * from './GetProjectsResponseDetailDto';
+export * from './GetProjectsResponseDto';
+export * from './GetProjectsResponseModelsDto';
+export * from './LabelDto';
+export * from './ModelSummaryDto';
+export * from './PaginationResponseGetProjectsResponseDetailDto';
+export * from './ProjectDetailDto';
+export * from './RoiRequestDto';
+export * from './RoiRequestPayload';
+export * from './RoiResponseDto';
+export * from './RoiResponsePayload';
+export * from './RoiSaveRequestDto';
+export * from './SubProjectResponseDto';
+export * from './SubProjectSummaryDto';
+export * from './TrainingResultRequestDto';
+export * from './UpdateProjectRequestDto';
+export * from './UploadRoisRequest';
