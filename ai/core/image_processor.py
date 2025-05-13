6ab92187e0e37fc7ac51d8ddb84b78ce76c76f41
TaegeunYou <dbxorms2310@naver.com>
2025-05-13 21:52:02 +0900

    [BE] fix: TISSUE 모델 전후처리 과정 변경

--- ai/core/image_processor.py
+++ ai/core/image_processor.py
@@ -8,11 +8,15 @@ from PIL import Image
 import subprocess
 import openslide
 import requests
+import json
+import uuid
 
 # 대용량 이미지 처리 가능하도록 설정
 Image.MAX_IMAGE_PIXELS = None  # DecompressionBombError 방지
+tile_size = 700
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "../datasets/data")
 ORIGIN_DIR = os.path.join(BASE_DIR, "../datasets/data/origin")
 RESULT_DIR = os.path.join(BASE_DIR, "../datasets/data/result")
 TEST_DIR = os.path.join(BASE_DIR, "../datasets/data/test")
@@ -20,11 +24,13 @@ TRAIN_DIR = os.path.join(BASE_DIR, "../datasets/data/train")
 VAL_DIR = os.path.join(BASE_DIR, "../datasets/data/val")
 MODEL_DIR = os.path.join(BASE_DIR, "../model")
 
-def process(request_data_list):
+def process(request_data):
     settings = app.state.settings
 
     # s3 초기화
-    s3 = boto3.client("s3", region_name=settings.AWS_REGION_NAME, aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+    s3 = boto3.client("s3",
+                      region_name=settings.AWS_REGION_NAME,
+                      aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
                       aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
     s3_bucket_name = settings.AWS_S3_BUCKET_NAME
 
@@ -36,17 +42,36 @@ def process(request_data_list):
     print("기존에 폴더에 있는 이미지랑 모델 삭제")
     delete_files()
 
+    # s3에서 svs 목록 로컬에 저장
+    print("s3에서 svs 목록 로컬에 저장")
+    download_svs(s3, s3_bucket_name, request_data)
+
+    if request_data["modelType"] in {"TISSUE", "MULTI"}:
+        tissue_process(request_data, s3, s3_bucket_name)
+
+    if request_data["modelType"] in {"CELL", "MULTI"}:
+        cell_process(request_data)
+
+    # 로컬에 다운받고 사용했던 이미지와 모델 삭제
+    delete_files()
+
+    # 응용 서버 호출
+    notify_application_server(request_data, settings)
+
+def tissue_process(request_data, s3, s3_bucket_name):
+    print("TISSUE 작업 실행")
+
     # sqs 메시지에서 제시해준 모델을 로컬에 저장
     print("sqs 메시지에서 제시해준 모델을 로컬에 저장")
-    download_model(s3, s3_bucket_name, request_data_list)
+    # download_model(s3, s3_bucket_name, request_data)
 
-    # s3에서 svs 파일이랑 타일링 ROI 목록 로컬에 저장.
-    print("s3에서 svs 파일이랑 타일링 ROI 목록 로컬에 저장")
-    download_original_images(s3, s3_bucket_name, request_data_list)
+    # s3에서 타일링 ROI 목록 로컬에 저장
+    print("s3에서 타일링 ROI 목록 로컬에 저장")
+    # download_tile_images(s3, s3_bucket_name, request_data)
 
     # roi 사진을 700 * 700 여러 개로 자르고 저장
     print("roi 사진을 700 * 700 여러 개로 자르고 저장")
-    split_and_save_images(request_data_list)
+    split_and_save_images(request_data)
 
     # 각 잘라진 사진 정보에 맞게 svs에서 해당 부분들 저장
     print("각 잘라진 사진 정보에 맞게 svs에서 해당 부분들 저장")
@@ -56,15 +81,18 @@ def process(request_data_list):
     print("test에 저장한 사진들을 train과 val로 복사")
     split_test_to_train_val()
 
+    # cmap.json 생성
+    generate_cmap_json(request_data)
+
     # 학습
-    if request_data_list["type"] == "TRAINING_INFERENCE":
+    if request_data["type"] == "TRAINING_INFERENCE":
         print("학습")
-        run_subprocess("train")
-        upload_model(s3, s3_bucket_name, request_data_list['model_name'])
+        run_subprocess("train", request_data)
+        upload_model(s3, s3_bucket_name, request_data)
 
     # 추론
     print("추론")
-    run_subprocess("predict")
+    run_subprocess("predict", request_data)
 
     # 추론 결과물들을 원래 같았던 roi 끼리 합치기
     print("추론 결과물들을 원래 같았던 roi 끼리 합치기")
@@ -72,15 +100,63 @@ def process(request_data_list):
 
     # 합친 roi들을 s3에 업로드
     print("합친 roi들을 s3에 업로드")
-    upload_images(s3, s3_bucket_name, request_data_list)
+    upload_images(s3, s3_bucket_name, request_data)
 
-    # 로컬에 다운받고 사용했던 이미지와 모델 삭제
-    delete_files()
+def cell_process(request_data):
+    print("CELL 작업 실행")
 
-    # 응용 서버 호출
-    notify_application_server(request_data_list, settings)
+    # svs에서 700*700 타일링 이미지 추출
+    cell_split_save_images(request_data)
+
+    # 모델 인풋 예상 : 사진 목록, 폴리곤 목록
+
+    #
+
+def download_svs(s3, s3_bucket_name, request_data):
+    for sub_project in request_data["subProjects"]:
+        sub_project_id = str(sub_project["subProjectId"])
+        svs_s3_path = sub_project["svsPath"]
+        svs_save_path = os.path.join(ORIGIN_DIR, "svs") + "/" + sub_project_id + ".svs"
+        s3.download_file(s3_bucket_name, svs_s3_path, svs_save_path)
+
+def cell_split_save_images(request_data):
+    input_folder = os.path.join(ORIGIN_DIR, "svs")
+    output_folder = os.path.join(TEST_DIR, "images")
+
+    for sub_project in request_data.get("subProjects", []):
+        sub_project_id = str(sub_project["subProjectId"])
+        svs_path = input_folder + "/" + sub_project_id + ".svs"
+        slide = openslide.OpenSlide(svs_path)
+
+        for roi in sub_project.get("roi", []):
+            roi_id = str(roi["roiId"])
+            base_name = f"{sub_project_id}_{roi_id}"
+            orig_x = roi["detail"]["x"]
+            orig_y = roi["detail"]["y"]
+            width = roi["detail"]["width"]
+            height = roi["detail"]["height"]
+
+            for y in range(0, height, tile_size):
+                for x in range(0, width, tile_size):
+                    new_x, new_y = orig_x + x, orig_y + y
+                    new_width = min(width - x, tile_size)
+                    new_height = min(height - y, tile_size)
+
+                    region = slide.read_region((new_x, new_y), level=0, size=(new_width, new_height))
+                    region = region.convert("RGB")  # RGBA -> RGB 변환
+
+                    tile_filename = f"{base_name}_{new_x}_{new_y}.png"
+                    output_image_path = os.path.join(output_folder, tile_filename)
+                    region.save(output_image_path)
+
+        slide.close()
+
 
 def ensure_dirs():
+    # data
+    for subfolder in ["origin", "result", "test", "train", "val"]:
+        os.makedirs(os.path.join(DATA_DIR,subfolder), exist_ok=True)
+
     # origin
     for subfolder in ["svs", "tile"]:
         os.makedirs(os.path.join(ORIGIN_DIR, subfolder), exist_ok=True)
@@ -95,25 +171,25 @@ def ensure_dirs():
         for subfolder in ["images", "labels"]:
             os.makedirs(os.path.join(folder, subfolder), exist_ok=True)
 
-def download_model(s3, s3_bucket_name, request_data_list):
-    s3_path = request_data_list["model_path"]
+def download_model(s3, s3_bucket_name, request_data):
+    s3_path = request_data["tissueModelPath"]
     save_path = os.path.join(MODEL_DIR, "checkpoints/model.pth")
     s3.download_file(s3_bucket_name, s3_path, save_path)
 
-def download_original_images(s3, s3_bucket_name, request_data_list):
-    # s3에서 svs 파일이랑 타일링 ROI 목록 로컬에 저장하기
-    svs_s3_path = request_data_list["svs_path"]
-    svs_save_path = os.path.join(ORIGIN_DIR, "svs") + "/basic.svs"
-    s3.download_file(s3_bucket_name, svs_s3_path, svs_save_path)
+def download_tile_images(s3, s3_bucket_name, request_data):
+    # s3에서 타일링 ROI 목록 로컬에 저장하기
+    for sub_project in request_data["subProjects"]:
+        sub_project_id = str(sub_project["subProjectId"])
 
-    for idx, roi in enumerate(request_data_list["roi"], start=1):
-        image_s3_path = roi["tissue_path"]
-        name = os.path.basename(image_s3_path)
-        image_save_path = os.path.join(ORIGIN_DIR, "tile") + "/" + name
-        s3.download_file(s3_bucket_name, image_s3_path, image_save_path)
+        for idx, roi in enumerate(sub_project["roi"], start=1):
+            image_s3_path = roi["tissuePath"]
+            basename = os.path.basename(image_s3_path)
+            extension = basename.rsplit('.')[1]
+            name = sub_project_id + "_" + str(roi["roiId"]) + "." + extension
+            image_save_path = os.path.join(ORIGIN_DIR, "tile") + "/" + name
+            s3.download_file(s3_bucket_name, image_s3_path, image_save_path)
 
-def split_and_save_images(request_data_list):
-    tile_size = 700
+def split_and_save_images(request_data):
     input_folder = os.path.join(ORIGIN_DIR, "tile")
     output_folder = os.path.join(TEST_DIR, "labels")
 
@@ -123,11 +199,17 @@ def split_and_save_images(request_data_list):
             image = Image.open(image_path)
 
             base_name = filename.rsplit(".")[0]
+            sub_project_id = base_name.split("_")[0]
+            sub_project = None
+            for tmp in request_data.get("subProjects", []):
+                if str(tmp.get("subProjectId")) == sub_project_id:
+                    sub_project = tmp
+                    break
+
             orig_x = orig_y = None
-            for image_info in request_data_list["roi"]:
-                s3_path = image_info["tissue_path"]
-                name = os.path.basename(s3_path)
-                if filename == name:
+            for image_info in sub_project.get("roi", []):
+                name = sub_project_id + "_" + str(image_info["roiId"])
+                if base_name == name:
                     orig_x = image_info["detail"]["x"]
                     orig_y = image_info["detail"]["y"]
                     break
@@ -140,18 +222,17 @@ def split_and_save_images(request_data_list):
                     box = (x, y, min(x + tile_size, width), min(y + tile_size, height))
                     tile = image.crop(box)
                     new_x, new_y = orig_x + x, orig_y + y  # 원본 사진 기준 좌표 계산
-                    tile_filename = f"{base_name}_{new_x}_{new_y}_anno.png"  # 원본 기준 좌표로 네이밍
+                    tile_filename = f"{base_name}_{new_x}_{new_y}_anno.png"
                     tile_path = os.path.join(output_folder, tile_filename)
                     tile.save(tile_path)
 
 def extract_svs_regions():
     labels_folder = os.path.join(TEST_DIR, "labels")
-    svs_folder = os.path.join(ORIGIN_DIR, "svs")
     output_folder = os.path.join(TEST_DIR, "images")
 
     for filename in os.listdir(labels_folder):
         if filename.lower().endswith(".png"):
-            name_without_ext = filename.rsplit(".", 1)[0]  # .png 제거
+            name_without_ext = filename.rsplit(".", 1)[0]
             base_name, x_str, y_str, _ = name_without_ext.rsplit("_", 3)
             x = int(x_str)
             y = int(y_str)
@@ -159,23 +240,24 @@ def extract_svs_regions():
             label_image = Image.open(label_image_path)
             tile_w, tile_h = label_image.size  # 잘라야 할 크기
 
-            svs_path = get_latest_svs_file(svs_folder)
+            sub_project_id = name_without_ext.split("_")[0]
+            svs_file_name = sub_project_id + ".svs"
+            svs_path = get_svs_file(svs_file_name)
+
             slide = openslide.OpenSlide(svs_path)
             region = slide.read_region((x, y), level=0, size=(tile_w, tile_h))
-            region = region.convert("RGB")  # RGBA -> RGB 변환
+            region = region.convert("RGB")
 
             output_image_path = os.path.join(output_folder, filename.replace("_anno", ""))
             region.save(output_image_path)
             slide.close()
 
-# .svs 파일 중 가장 최근에 생성된 파일 경로 가져오기
-def get_latest_svs_file(svs_folder):
-    svs_files = glob.glob(os.path.join(svs_folder, "*.svs"))
-    if not svs_files:
-        return None  # svs 파일 없음
-    # 최근 수정 시간 기준으로 정렬하여 가장 마지막 파일 선택
-    latest_file = max(svs_files, key=os.path.getmtime)
-    return latest_file
+def get_svs_file(filename):
+    svs_folder = os.path.join(ORIGIN_DIR, "svs")
+    target_path = os.path.join(svs_folder, filename)
+    if os.path.isfile(target_path):
+        return target_path
+    return None
 
 def split_test_to_train_val():
     test_images_folder = os.path.join(TEST_DIR, "images")
@@ -203,9 +285,28 @@ def split_test_to_train_val():
 
     print(f"Split completed: {len(train_files)} train images, {len(val_files)} val images")
 
-def run_subprocess(mode: str):
+def generate_cmap_json(request_data):
+    labels = request_data.get("labels", [])
+    cmap = []
+
+    for label in labels:
+        cmap.append({
+            "label": label["classIndex"] + 1,
+            "rgb": label["color"]
+        })
+
+    # JSON 파일로 저장
+    output_path = os.path.join(BASE_DIR, "../model")
+    file_path = os.path.join(output_path, "cmap.json")
+
+    with open(file_path, "w") as f:
+        json.dump(cmap, f, indent=2)
+
+    print("cmap.json 저장 완료")
+
+def run_subprocess(mode: str, request_data):
     """ 학습 또는 예측을 실행하는 함수 """
-    command = get_command(mode)  # mode에 따라 명령어 가져오기
+    command = get_command(mode, request_data)  # mode에 따라 명령어 가져오기
     process = subprocess.Popen(
         command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, bufsize=1
     )
@@ -219,15 +320,15 @@ def run_subprocess(mode: str):
     return stdout
 
 # 학습 및 예측에 공통으로 사용하는 명령어 생성 함수
-def get_command(mode: str):
+def get_command(mode: str, request_data):
     """ 학습(train) 또는 예측(predict) 명령어를 생성하는 함수 """
+    num_classes = str(len(request_data["labels"]) + 1)
 
     if mode == "train":
-        return [
+        command = [
             "python", os.path.join(BASE_DIR, "../model/main.py"),
             "--data_root", os.path.join(BASE_DIR, "../datasets/data"),
             "--dataset", "cityscapes",
-            "--num_classes", "2",
             "--model", "deeplabv3plus_resnet101",
             "--total_itrs", "1000",
             "--gpu_id", "mps",
@@ -237,9 +338,14 @@ def get_command(mode: str):
             "--batch_size", "8",
             "--output_stride", "16",
             "--cmap_file", os.path.join(MODEL_DIR, "cmap.json"),
-            "--num_classes", "2", # TODO 프론트가 주는 클래스 갯수, +1 (배경)
-            "--gpu_id", "0",
+            "--num_classes", num_classes,
+            "--gpu_id", "0"
         ]
+        if request_data.get("tissueModelPath"):  # None 또는 빈 문자열이 아닌 경우에만 추가
+            checkpoint_dir = os.path.join(MODEL_DIR, "checkpoints")
+            latest_checkpoint = get_latest_checkpoint(checkpoint_dir)
+            command += ["--ckpt", latest_checkpoint]
+        return command
     elif mode == "predict":
         checkpoint_dir = os.path.join(MODEL_DIR, "checkpoints")
         latest_checkpoint = get_latest_best_checkpoint(checkpoint_dir)
@@ -251,12 +357,12 @@ def get_command(mode: str):
             "--ckpt", latest_checkpoint,
             "--save_val_results_to", os.path.join(RESULT_DIR, "tile/"),
             "--cmap_file", os.path.join(MODEL_DIR, "cmap.json"),
-            "--num_classes", "2", # TODO 여기에 프론트가 주는 클래스 갯수, +1 (배경)
+            "--num_classes", num_classes
         ]
     else:
         raise ValueError("Invalid mode: choose 'train' or 'predict'")
 
-def get_latest_best_checkpoint(checkpoint_dir):
+def get_model(checkpoint_dir):
     """ 'best_'로 시작하는 .pth 파일 중 가장 최근에 수정된 파일 반환 """
     best_pth_files = glob.glob(os.path.join(checkpoint_dir, "best_*.pth"))
 
@@ -265,6 +371,29 @@ def get_latest_best_checkpoint(checkpoint_dir):
 
     latest_best_pth = max(best_pth_files, key=os.path.getmtime)
     return latest_best_pth
+
+def get_latest_checkpoint(checkpoint_dir):
+    """ 모든 .pth 파일 중에서 가장 최근에 수정된 파일 반환 """
+    pth_files = glob.glob(os.path.join(checkpoint_dir, "*.pth"))
+
+    if not pth_files:
+        raise FileNotFoundError(f"체크포인트 폴더({checkpoint_dir})에 .pth 파일이 없습니다!")
+
+    latest_pth = max(pth_files, key=os.path.getmtime)
+    return latest_pth
+
+def get_latest_best_checkpoint(checkpoint_dir):
+    best_pth_files = glob.glob(os.path.join(checkpoint_dir, "best_*.pth"))
+    if best_pth_files:
+        latest_best_pth = max(best_pth_files, key=os.path.getmtime)
+        return latest_best_pth
+
+    pth_files = glob.glob(os.path.join(checkpoint_dir, "*.pth"))
+    if not pth_files:
+        raise FileNotFoundError(f"체크포인트 폴더({checkpoint_dir})에 .pth 파일이 없습니다!")
+
+    latest_pth = max(pth_files, key=os.path.getmtime)
+    return latest_pth
 
 def merge_images():
     input_folder = os.path.join(RESULT_DIR, "tile")
@@ -315,36 +444,47 @@ def merge_images():
         merged_image.save(output_path)
         print(f"Merged image saved: {output_path}")
 
-def upload_images(s3, s3_bucket_name, request_data_list):
+def upload_images(s3, s3_bucket_name, request_data):
     result_path = os.path.join(RESULT_DIR, "roi")
-    allowed_extensions = {'.jpg', '.jpeg', '.png', '.bmp', '.tif', '.tiff'}
 
-    sub_project_id = request_data_list["sub_project_id"]
-    annotation_history_id = request_data_list["annotation_history_id"]
+    for sub_project in request_data.get("subProjects", []):
+        sub_project_id = str(sub_project.get("subProjectId"))
+        annotation_history_id = sub_project.get("annotationHistoryId")
 
-    for filename in os.listdir(result_path):
-        file_path = os.path.join(result_path, filename)
+        for roi in sub_project.get("roi", []):
+            roi_id = str(roi.get("roiId"))
 
-        if os.path.isfile(file_path):
-            _, ext = os.path.splitext(filename)
-            if ext.lower() in allowed_extensions:
+            for filename in os.listdir(result_path):
+                file_path = os.path.join(result_path, filename)
+
                 s3_key = (
                     f"sub-project/{sub_project_id}/"
                     f"annotation-history/{annotation_history_id}/"
                     f"inference/{filename}"
                 )
+                s3_path = f"s3://{s3_bucket_name}/{s3_key}"
+                print(f"업로드: {s3_path}")
                 s3.upload_file(file_path, s3_bucket_name, s3_key)
 
-def upload_model(s3, s3_bucket_name, model_name):
+                roi["tissuePath"] = s3_key
+
+
+def upload_model(s3, s3_bucket_name, request_data):
     checkpoint_dir = os.path.join(MODEL_DIR, "checkpoints")
     file_path = get_latest_best_checkpoint(checkpoint_dir)  # 최신 pth 파일 가져오기
-
+    model_name = f"{uuid.uuid4()}.pth"
     if os.path.isfile(file_path):
         s3_key = f"model/{model_name}"
         s3.upload_file(file_path, s3_bucket_name, s3_key)
+        request_data["tissueModelPath"] = f"models/{model_name}",
 
 def delete_files():
     print("Deleting local files")
+
+    # cmap.json
+    cmap_path = os.path.join(MODEL_DIR, "cmap.json")
+    if os.path.exists(cmap_path):
+        os.remove(cmap_path)
 
     # model
     folder_path = os.path.join(MODEL_DIR, "checkpoints")
@@ -371,22 +511,16 @@ def delete_files():
             for file in os.listdir(folder_path):
                 os.remove(os.path.join(folder_path, file))
 
-def notify_application_server(request_data_list, settings):
+def notify_application_server(request_data, settings):
     try:
-        application_server_url = f"{settings.APPLICATION_SERVER_URL.rstrip('/')}/api/model-server/training/result"
-
-        payload = {
-            "sub_project_id": request_data_list["sub_project_id"],
-            "annotation_history_id": request_data_list["annotation_history_id"],
-            "model_path": request_data_list["model_path"],
-            "roi": request_data_list["roi"]
-        }
+        project_id = request_data["projectId"]
+        application_server_url = f"{settings.APPLICATION_SERVER_URL}/api/model-server/projects/{project_id}/model-results"
 
         headers = {
             "Authorization": f"Bearer {settings.APPLICATION_SERVER_TOKEN}"
         }
 
-        response = requests.post(application_server_url, json=payload, headers=headers, timeout=5)
+        response = requests.post(application_server_url, json=request_data, headers=headers, timeout=10)
 
         if response.ok:
             print("응용 서버에 학습 결과 전송 성공")
@@ -394,5 +528,4 @@ def notify_application_server(request_data_list, settings):
             print(f"응용 서버 응답 오류: {response.status_code}, {response.text}")
 
     except Exception as e:
-        print(f" Spring API 호출 실패 (무시됨): {str(e)}")
-
+        print(f"Spring API 호출 실패: {str(e)}")
