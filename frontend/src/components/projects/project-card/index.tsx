adb71d44916db68fbbc3d1a51ad6afea0e26bc96
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2025-05-14 04:33:23 +0900

    [FE] feat: 팝오버 시 프로젝트 수정 / 삭제 Api 연결 및 코드 리팩토링

--- frontend/src/components/projects/project-card/index.tsx
+++ frontend/src/components/projects/project-card/index.tsx
@@ -16,43 +16,75 @@ import {
   Share2,
   Trash2,
 } from 'lucide-react';
-import { Project, SubProject } from '@/types/project-schema';
-import { formatDateTime } from '@/lib/utils';
-import { useRef, useState } from 'react';
+import { useMemo, useRef, useState } from 'react';
 import ProjectEditModal from '@/components/projects/project-edit-modal';
 import ProjectDeleteModal from '@/components/projects/poject-delete-modal';
+import { ProjectAPIApi, GetProjectsResponseDetailDto } from '@/generated-api';
+import { toast } from 'sonner';
 
 interface ProjectCardProps {
-  project: Project;
-  subProjects: SubProject[];
+  project: GetProjectsResponseDetailDto;
+  refetchProjects: () => void;
 }
 
 export default function ProjectCard({
   project,
-  subProjects,
+  refetchProjects,
 }: ProjectCardProps) {
-  // TODO: 프로젝트 수정, 삭제 (모달) Api 연동
-
+  // TODO: 프로젝트 수정 에러 해결
+  const projectApi = useMemo(() => new ProjectAPIApi(), []);
   const router = useRouter();
-  const thumbnails = subProjects
-    .filter((sp) => sp.projectId === String(project.id))
-    .slice(0, 4);
 
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
-
-  const created = formatDateTime(project.createdAt);
-  const updated = formatDateTime(project.updatedAt);
-
   const [editOpen, setEditOpen] = useState(false);
   const [deleteOpen, setDeleteOpen] = useState(false);
 
   const suppressClickRef = useRef(false);
 
-  const handleSuppressClick = () => {
+  const blockNavigationRef = () => {
     suppressClickRef.current = true;
     setTimeout(() => {
       suppressClickRef.current = false;
     }, 300);
+  };
+
+  const closeModals = () => {
+    blockNavigationRef();
+    setEditOpen(false);
+    setDeleteOpen(false);
+    setIsPopoverOpen(false);
+  };
+
+  const handleUpdateProject = async (title: string, description: string) => {
+    try {
+      await projectApi.updateProject({
+        projectId: project.projectId || -1,
+        updateProjectRequestDto: { title, description },
+      });
+
+      toast.success('The project has been updated successfully.');
+      refetchProjects();
+    } catch (error) {
+      toast.error('Failed to update the project. Please try again.');
+    } finally {
+      blockNavigationRef();
+      setEditOpen(false);
+      setIsPopoverOpen(false);
+    }
+  };
+
+  const handleDeleteProject = async () => {
+    try {
+      await projectApi.deleteProject({ projectId: project.projectId || -1 });
+      toast.success('The project has been deleted successfully.');
+    } catch (error) {
+      toast.error('Failed to delete the project. Please try again.');
+    } finally {
+      blockNavigationRef();
+      setDeleteOpen(false);
+      setIsPopoverOpen(false);
+      refetchProjects();
+    }
   };
 
   return (
@@ -63,26 +95,33 @@ export default function ProjectCard({
           e.stopPropagation();
           return;
         }
-        router.push(`/main/projects/${project.id}`);
+        router.push(`/main/projects/${project.projectId}`);
       }}
       className="flex cursor-pointer flex-row items-center gap-3 rounded-lg border bg-white py-4 pl-3 pr-1 shadow-sm transition-shadow hover:shadow-md"
     >
       <div className="grid shrink-0 grid-cols-2 gap-1">
-        {[0, 1, 2, 3].map((i) => {
-          const thumb = thumbnails[i];
-          return thumb ? (
-            <Image
-              key={i}
-              src={thumb.thumbnail}
-              alt="Thumbnail"
-              width={50}
-              height={50}
-              className="size-11 rounded object-cover"
-            />
-          ) : (
-            <div key={i} className="bg-muted size-11 rounded" />
-          );
-        })}
+        {(project.thumbnailUrl && project.thumbnailUrl.length > 0
+          ? project.thumbnailUrl
+          : []
+        ).map((url, index) => (
+          <Image
+            key={index}
+            src={url}
+            alt="Thumbnail"
+            width={50}
+            height={50}
+            className="size-11 rounded object-cover"
+          />
+        ))}
+
+        {Array.from({
+          length: Math.max(0, 4 - (project.thumbnailUrl?.length || 0)),
+        }).map((_, index) => (
+          <div
+            key={`placeholder-${index}`}
+            className="bg-muted size-11 rounded"
+          />
+        ))}
       </div>
 
       <div className="-mt-1 flex w-full flex-col">
@@ -93,7 +132,7 @@ export default function ProjectCard({
               variant="secondary"
               className="inline-block max-w-[12ch] overflow-hidden truncate whitespace-nowrap"
             >
-              {project.modelNameList[0]}
+              {project.modelName}
             </Badge>
           </div>
 
@@ -105,7 +144,6 @@ export default function ProjectCard({
                 className="text-muted-foreground hover:text-primary cursor-pointer hover:bg-white"
                 onClick={(e) => {
                   e.stopPropagation();
-                  setIsPopoverOpen(true);
                 }}
               >
                 <EllipsisVertical className="h-3 w-2" />
@@ -123,7 +161,9 @@ export default function ProjectCard({
                     icon: <PenTool className="h-4 w-4" />,
                     label: 'Annotation',
                     onClick: () =>
-                      router.push(`/main/projects/annotation/${project.id}`),
+                      router.push(
+                        `/main/projects/annotation/${project.projectId}`,
+                      ),
                   },
                   {
                     icon: <Pencil className="h-4 w-4" />,
@@ -167,42 +207,28 @@ export default function ProjectCard({
 
         <h3 className="text-md font-bold">{project.title}</h3>
         <p className="text-muted-foreground text-xs">
-          Created At : {created.full}
+          Created : {project.createdAt}
         </p>
         <p className="text-muted-foreground text-xs">
-          Updated At : {updated.full}
+          Edited : {project.updatedAt}
         </p>
       </div>
 
       <ProjectEditModal
         open={editOpen}
-        onClose={() => {
-          handleSuppressClick();
-          setEditOpen(false);
-          setIsPopoverOpen(false);
+        onClose={closeModals}
+        onClickEdit={async ({ title, description }) => {
+          await handleUpdateProject(title, description);
         }}
-        onClickEdit={() => {
-          handleSuppressClick();
-          setEditOpen(false);
-          setIsPopoverOpen(false);
-        }}
-        initialTitle={project.title}
-        initialDescription={project.description}
+        initialTitle={project.title || ''}
+        initialDescription={project.description || ''}
       />
 
       <ProjectDeleteModal
         open={deleteOpen}
-        onClose={() => {
-          handleSuppressClick();
-          setDeleteOpen(false);
-          setIsPopoverOpen(false);
-        }}
-        projectTitle={project.title}
-        onClickDelete={() => {
-          handleSuppressClick();
-          setDeleteOpen(false);
-          setIsPopoverOpen(false);
-        }}
+        onClose={closeModals}
+        projectTitle={project.title || ''}
+        onClickDelete={handleDeleteProject}
       />
     </div>
   );
