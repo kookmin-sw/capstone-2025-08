eb8380b6038f297db624d2f9e3052ece1fa7362f
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2025-05-15 16:52:24 +0900

    [FE] chore: openapi generator 업데이트

--- frontend/src/generated-api/apis/index.ts
+++ frontend/src/generated-api/apis/index.ts
@@ -1,7 +1,5 @@
 /* tslint:disable */
 /* eslint-disable */
-export * from './AnnotationHistoryAPIApi';
 export * from './ModelServerAPIApi';
 export * from './ProjectAPIApi';
 export * from './ProjectAnnotationAPIApi';
-export * from './SubProjectAPIApi';
