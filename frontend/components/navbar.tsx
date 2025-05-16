536e4969f8777f04547c15cc371ecacafb74c06d
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2025-05-17 07:58:22 +0900

    [FE] chore: 뭐가문제니

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -11,9 +11,9 @@ import {PathosLogoHorizontal} from "@/components/pathos-logo-horizontal";
 
 const navItems = [
   { name: "Intro", href: "/" },
-  { name: "Features", href: "/features.html" },
-  { name: "Docs", href: "/docs.html" },
-  { name: "Team", href: "/team.html" },
+  { name: "Features", href: "/features" },
+  { name: "Docs", href: "/docs" },
+  { name: "Team", href: "/team" },
 ]
 
 export default function Navbar() {
